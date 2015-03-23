624828c176babb720e71dd3c7d3f910783573f7d
Nicolas Waissbluth <nwaissbluth@gmail.com>
2015-03-23 00:02:05 -0300

    Analytics code.

--- app/assets/javascripts/tracking.js
+++ app/assets/javascripts/tracking.js
@@ -3,6 +3,6 @@
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-XXXXXXXX-1', 'auto');
+ga('create', 'UA-61068424-1', 'auto');
 ga('send', 'pageview');
 ga('set', 'dimension1', window.devicePixelRatio || 1);
