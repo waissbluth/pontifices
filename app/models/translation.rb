67aa7d01ef850a831c1d431fc3b7a4b3561e7353
Nicolas Waissbluth <nwaissbluth@gmail.com>
2015-03-22 23:45:03 -0300

    Migration.

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,2 +1,5 @@
 class Translation < ActiveRecord::Base
+  validates [:papa, :pontifice], presence: true
+
+  scope :active, -> {where(active: true)}
 end
--- db/migrate/20150323020612_create_translations.rb
+++ db/migrate/20150323020612_create_translations.rb
@@ -3,7 +3,7 @@ class CreateTranslations < ActiveRecord::Migration
     create_table :translations do |t|
       t.string :papa
       t.string :pontifice
-      t.boolean :active
+      t.boolean :active, default: false, null: false
 
       t.timestamps null: false
     end
