ebfcd0ea9f46817c329a027fe44a680d4233e686
Nicolas Waissbluth <nwaissbluth@gmail.com>
2015-03-22 23:38:52 -0300

    Haml scaffold. Locale fix.

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module Pontifice
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = [:es]
     config.i18n.default_locale = :es
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
