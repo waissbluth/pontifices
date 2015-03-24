5773a02657ed2883d3f3d9307a2fc7a0ed2e422e
Nicolas Waissbluth <nwaissbluth@gmail.com>
2015-03-24 12:21:43 -0300

    Correct validation.

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,5 +1,6 @@
 class Translation < ActiveRecord::Base
   validates :papa, presence: true, uniqueness: true
+  validates :pontifice, presence: true, if: :active
 
   scope :active, -> { where(active: true) }
   scope :random, -> { order('random()') }
