d6f25d8ed8a2510db163dcae5998a8de13b8b48d
Nicolas Waissbluth <nwaissbluth@gmail.com>
2015-03-26 19:08:39 -0300

    Unique validation. Order in admin.

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,8 +1,9 @@
 class Translation < ActiveRecord::Base
   validates :papa, presence: true
   validates :pontifice, presence: true, if: :active
+  validates :pontifice, uniqueness: {scope: :papa}
 
   scope :active, -> { where(active: true) }
   scope :random, -> { order('random()') }
-  scope :sorted, -> { order(:papa)}
+  scope :sorted, -> { order(:active, :papa)}
 end
