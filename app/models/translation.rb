55c37cd38d904a7a622f83228ad8ce6dcca0d44b
Nicolas Waissbluth <nwaissbluth@gmail.com>
2015-03-26 22:50:25 -0300

    Safe random implemented.

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -5,6 +5,24 @@ class Translation < ActiveRecord::Base
 
   scope :active, -> { where(active: true) }
   scope :random, -> { order('random()') }
-  scope :safe_random, -> { order('allowed_first, random()') }
   scope :sorted, -> { order(:active, :papa)}
+
+  def self.safe_random
+    result = []
+    pending = []
+    rest = []
+    random.each do |t|
+      if result.length >=5
+        rest.push(t)
+      else
+        if t.allowed_first
+          result.push(t)
+        else
+          pending.push(t)
+        end
+      end
+    end
+    result + pending + rest
+  end
+
 end
